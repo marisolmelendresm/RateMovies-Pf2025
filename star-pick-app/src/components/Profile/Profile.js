89af1c1fa5e2acf318f42bd63cb0fe6f725c19d5
Marisol Melendres <marisolmm@Marisols-MacBook-Air.local>
2025-12-16 20:40:53 -0600

    Profile page skeleton

--- star-pick-app/src/components/Profile/Profile.js
+++ star-pick-app/src/components/Profile/Profile.js
@@ -1,9 +1,25 @@
+import MovieCatalog from '../MovieCatalog/MovieCatalog';
 import './Profile.css';
 
 function Profile() {
+    const watchCount = 0;
+
     return (
         <div>
-            <h2>Profile Page Coming Soon!</h2>
+            <div className="greetingContainer">
+                <h2 className="greeting">Hey,</h2> 
+                <h2 className="name">Marisol Melendres</h2>
+            </div>
+            <div className="watchCountContainer">
+                <p className="watchText">You've watched</p>
+                <p className="watchCount">{watchCount}</p>
+                <p className="watchText">movies this year</p>
+            </div>
+            <div className="movieCatalogContainer">
+                <MovieCatalog categoryName="Rocky" />
+                <MovieCatalog categoryName="Avengers" />
+            </div>
+
         </div>
     );
 }
